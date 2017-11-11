ff9359ddce0842b66e22f19f3e2fa6761e419df9
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-11-11 00:30:43 -0800

    init repo with backend flask boilerplate with PSQL connection and index.html served

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv(), override=True)
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("PSQLURL")
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200))
+    complete = db.Column(db.Boolean)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
